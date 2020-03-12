c7893eac35193a478142d16c306a8491f9d283fa
nadianadine16 <nadianadine012@gmail.com>
2020-03-12 11:09:38 +0700

    first commit

---
+++ app/src/main/java/id/ac/polinema/recyclerview/models/TeamLogo.java
@@ -0,0 +1,28 @@
+package id.ac.polinema.recyclerview.models;
+
+public class TeamLogo {
+
+    public String logo;
+    public  String name;
+
+    public TeamLogo(String logo, String name) {
+        this.logo = logo;
+        this.name = name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
